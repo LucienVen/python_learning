e17285c2ff3efb126684f3ef0d0ba2e5a11965f4
LucienVen <liangliang.too@gmail.com>
2017-11-08 23:49:27 +0800

    添加尾结点引用域简单链表实现

--- part3/SingleLinkedList.py
+++ part3/SingleLinkedList.py
@@ -112,18 +112,59 @@ class LList:
 
 
 
+# 继承和扩充定义新链表类
+class LList1(LList):
+    def __init__(self):
+        LList.__init__(self)
+        self._rear = None
+
+    # 覆盖prepend方法，表首插入元素
+    def prepend(self, elem):
+        if self._head is None:
+            self._head = LNode(elem, self._head)
+            self._rear = self._head
+        else:
+            self._head = LNode(elem, self._head)
+
+    # append，表尾插入元素
+    def append(self, elem):
+        if self._head is None:
+            self._head = LNode(elem, self._head)
+            self._rear = self._head
+        else:
+            self._rear.next = LNode(elem)
+            self._rear = self._rear.next
+
+    # 弹出末元素的操作pop_last
+    def pop_last(self):
+        # 判断是否为空表
+        if self._head is None:
+            raise LinkedListUnderflow("in pop_last")
+        p = self._head
+        # 如果表只有一个元素
+        if p.next is None:
+            e = p.elem
+            self._head = None
+            return e
+        # 直到p.next是最后结点
+        while p.next.next is not None:
+            p = p.next
+        
+        e = p.next.elem
+        self._rear = p
+        return e
+        
+        
+
+
 # 对链表的简单操作
-mlist1 = LList()
-for i in range(10):
-    mlist1.prepend(i)
-for i in range(10):
-    mlist1.append(i)
-mlist1.printAll()
+# mlist1 = LList()
+# for i in range(10):
+#     mlist1.prepend(i)
+# for i in range(10):
+#     mlist1.append(i)
+# mlist1.printAll()
 # mlist1.for_each(print)
-
-
-
-
 
 
 
